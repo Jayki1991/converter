584237e3d6b24bb8fa2e8d0ad2a376b61922a72f
J. Kirsch <htw.jukirsch@gmail.com>
2017-07-23 23:50:59 +0200

    Funktionalität wurde implementiert. Programm ist jetzt nutzbar und Kommentare wurde eingesetzt (besser)

--- convert.py
+++ convert.py
@@ -123,7 +123,7 @@ class Fenster(QMainWindow):
         self.pfad = openData
 
     def convert(self):
-        # Funktionalität
+        # Funktionalitäten
         items = self.area.itemList.findItems('.', QtCore.Qt.MatchContains)
         for i in items:
             self.area.itemList.setCurrentItem(i)
